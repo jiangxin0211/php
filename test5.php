80a8f983037f30a9f12d6d6635ca0e7b6c055941
jiangxin0211 <jiangxin0211@163.com>
2017-12-07 21:33:48 +0800

    test

---
+++ test5.php
@@ -0,0 +1,39 @@
+<?php
+//九九乘法表 for while do-while
+
+    for($i=1;$i<=9;$i++;){
+        for($j=1;$j<$i;$j++;){
+            echo $i. "*" .$j. "=" .($i * $j)." ";
+        }
+        echo "<br />";
+    }
+
+    
+    echo "<table width=800 border=1 align='center'>";
+    echo "<tr>";
+    for($i=1;$i<=9;$i++;){
+        for($j=1;$j<$i;$j++;){
+            echo "<td>";
+            echo $i. "*" .$j. "=" .($i * $j)." ";
+            echo "</td>";
+        }
+        echo "</tr>";
+        echo "<br />";
+    }
+
+    //while
+
+    $i=1;
+    while($i<=9){
+        $j=1;
+        while($j<$i){
+            echo $i. "*" .$j. "=" .($i * $j)." ";
+
+        }
+    }
+
+
+
+
+
+?>
